802d5744ce1e62dd7a6f5357052ecbd105431bb5
Andrew Purchase <andpurch@gmail.com>
2021-03-30 14:12:21 -0500

    ...

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Courses(models.Model):
-    course_id = models.CharField(max_length=10)
+    course_id = models.CharField(max_length=10, primary_key=True)
     description = models.CharField(max_length=50)
     def __str__(self):
         return self.course_id
@@ -10,14 +10,20 @@ class Courses(models.Model):
 class Saved_Data(models.Model):
     course_id = models.CharField(max_length=10)
     position = models.IntegerField()
+    def __str__(self):
+        return self.course_id
     #label divs as '1, 2, 3, 4'... so 1 is a course saved in
     #the available courses div, and 2 is the first semester, and so on
 
 class Semesters(models.Model):
-    semester_id = models.CharField(max_length=10)
+    semester_id = models.CharField(max_length=10, primary_key=True)
     title = models.CharField(max_length=10)
+    def __str__(self):
+        return self.title
+
 
 class Offered_In(models.Model):
-    semester_id = models.CharField(max_length=10)
-    CS1000 = models.IntegerField()
-    CS1010 = models.IntegerField()
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    semester = models.ForeignKey(Semesters, on_delete=models.CASCADE)
+    # semester_id = models.CharField(max_length=10)
+
