1cd0304562ded185b71bcfe2809f2062832468ad
Andrew Purchase <andpurch@gmail.com>
2021-04-01 10:21:14 -0500

    ...

--- planner/models.py
+++ planner/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Courses(models.Model):
+class Course(models.Model):
     course_id = models.CharField(max_length=10, primary_key=True)
     description = models.CharField(max_length=50)
     def __str__(self):
@@ -15,7 +15,7 @@ class Saved_Data(models.Model):
     #label divs as '1, 2, 3, 4'... so 1 is a course saved in
     #the available courses div, and 2 is the first semester, and so on
 
-class Semesters(models.Model):
+class Semester(models.Model):
     semester_id = models.CharField(max_length=10, primary_key=True)
     title = models.CharField(max_length=10)
     def __str__(self):
