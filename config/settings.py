750b3b98a14a3c33905b76bf6c124000e1b92bb9
Andrew Purchase <andpurch@gmail.com>
2021-04-02 09:58:37 -0500

    ...

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'planner',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,25 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
+
+from django.urls import path
+
+from planner import views
+# from django.urls import path
+
+from django.contrib import admin
+from django.urls import path
+
+from planner import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('planner/', include('planner.urls')), # new
     path('planner/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('admin/', admin.site.urls),
+    path('courses', views.course_list),
+    path('semesters', views.semester_list),
+    path('offered_in', views.offered_in_list),
+    path('courses/offered_in/<slug:cid>', views.offered_in_course),
 ]
